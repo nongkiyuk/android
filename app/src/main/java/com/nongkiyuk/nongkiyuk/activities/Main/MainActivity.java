b23a984f6c988b45e92d4b5610907b31f1f1f01b
maful <mafulprayoga@gmail.com>
2019-01-05 09:15:32 +0700

    Create Fragment

--- app/src/main/java/com/nongkiyuk/nongkiyuk/activities/Main/MainActivity.java
+++ app/src/main/java/com/nongkiyuk/nongkiyuk/activities/Main/MainActivity.java
@@ -1,15 +1,53 @@
 package com.nongkiyuk.nongkiyuk.activities.Main;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.nongkiyuk.nongkiyuk.R;
+import com.nongkiyuk.nongkiyuk.activities.Account.AccountFragment;
+import com.nongkiyuk.nongkiyuk.activities.Favorite.FavoriteFragment;
+import com.nongkiyuk.nongkiyuk.activities.Home.HomeFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new HomeFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.home_menu:
+                fragment = new HomeFragment();
+                break;
+            case R.id.favorite_menu:
+                fragment = new FavoriteFragment();
+                break;
+            case R.id.account_menu:
+                fragment = new AccountFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
