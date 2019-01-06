bf4dec9fde6c0d788a790c48257a8e4e3107992a
Aji Kamaludin <aji19kamaludin@gmail.com>
2019-01-06 22:40:29 +0700

    Fix Notification: Working On O and under O , notification with sound and click go to sigup activities

--- app/src/main/java/com/nongkiyuk/nongkiyuk/network/FirebaseMessagingClientService.java
+++ app/src/main/java/com/nongkiyuk/nongkiyuk/network/FirebaseMessagingClientService.java
@@ -1,22 +1,81 @@
 package com.nongkiyuk.nongkiyuk.network;
 
-import android.os.Bundle;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.nongkiyuk.nongkiyuk.R;
+import com.nongkiyuk.nongkiyuk.activities.SignUp.SignupActivity;
+import com.nongkiyuk.nongkiyuk.utils.NotificationUtils;
+
+import static com.nongkiyuk.nongkiyuk.utils.NotificationUtils.ANDROID_CHANNEL_ID;
 
 public class FirebaseMessagingClientService extends FirebaseMessagingService {
-    private static final String TAG = "Firebase Client Service";
+    private static final String TAG = "Notification";
 
     public FirebaseMessagingClientService() {
+        Log.d(TAG, "Messeging Called");
     }
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: ");
-        // TODO: Make a Notification if Recive a Notification From FCM
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " +
+                    remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+    }
+
+    private void sendNotification(String messageTitle, String messageBody) {
+        Intent intent = new Intent(this, SignupActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Notification.Builder notifBuilder = new Notification.Builder(getApplicationContext(), ANDROID_CHANNEL_ID)
+                    .setSmallIcon(R.drawable.nongkiyuk_logo)
+                    .setContentTitle(messageTitle)
+                    .setContentText(messageBody)
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent);
+
+            NotificationUtils utils = new NotificationUtils(getApplicationContext());
+            utils.getManager().notify(0, notifBuilder.build());
+        }else{
+
+            NotificationCompat.Builder notificationBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.drawable.nongkiyuk_logo)
+                            .setContentTitle(messageTitle)
+                            .setContentText(messageBody)
+                            .setAutoCancel(true)
+                            .setSound(defaultSoundUri)
+                            .setContentIntent(pendingIntent);
+
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(0, notificationBuilder.build());
+        }
+
+
     }
 }
