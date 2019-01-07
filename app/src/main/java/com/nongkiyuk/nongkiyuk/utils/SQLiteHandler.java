7f48ae9ce73d67fb5d5218bd4390378af097b5ab
maful <mafulprayoga@gmail.com>
2019-01-07 07:18:37 +0700

    Membuat class SQLite Helper

---
+++ app/src/main/java/com/nongkiyuk/nongkiyuk/utils/SQLiteHandler.java
@@ -0,0 +1,108 @@
+package com.nongkiyuk.nongkiyuk.utils;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.HashMap;
+
+public class SQLiteHandler extends SQLiteOpenHelper {
+    private static final String TAG = SQLiteHandler.class.getSimpleName();
+
+    // All Static variables
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "nongki_android_api";
+
+    // Login table name
+    private static final String TABLE_USER = "users";
+
+    // Login Table Columns names
+    private static final String KEY_ID = "id";
+    private static final String KEY_ACCESS_TOKEN = "name";
+    private static final String KEY_TOKEN_TYPE = "email";
+    private static final String KEY_EXPIRES_AT = "created_at";
+
+    public SQLiteHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_LOGIN_TABLE = "CREATE TABLE " + TABLE_USER + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_ACCESS_TOKEN + " TEXT,"
+                + KEY_TOKEN_TYPE + " TEXT," + KEY_EXPIRES_AT + " TEXT" + ")";
+        db.execSQL(CREATE_LOGIN_TABLE);
+
+        Log.d(TAG, "Database tables created");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+
+        // Create tables again
+        onCreate(db);
+    }
+
+    /**
+     * Storing user details in database
+     * */
+    public void addUser(String access_token, String token_type, String expires_at) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_ACCESS_TOKEN, access_token);
+        values.put(KEY_TOKEN_TYPE, token_type);
+        values.put(KEY_EXPIRES_AT, expires_at);
+
+        // Inserting Row
+        long id = db.insert(TABLE_USER, null, values);
+        db.close(); // Closing database connection
+
+        Log.d(TAG, "New user inserted into sqlite: " + id);
+    }
+
+    /**
+     * Getting user data from database
+     * */
+    public HashMap<String, String> getUserDetails() {
+        HashMap<String, String> user = new HashMap<String, String>();
+        String selectQuery = "SELECT  * FROM " + TABLE_USER;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // Move to first row
+        cursor.moveToFirst();
+        if (cursor.getCount() > 0) {
+            user.put("access_token", cursor.getString(1));
+            user.put("token_type", cursor.getString(2));
+            user.put("expires_at", cursor.getString(3));
+        }
+        cursor.close();
+        db.close();
+        // return user
+        Log.d(TAG, "Fetching user from Sqlite: " + user.toString());
+
+        return user;
+    }
+
+    /**
+     * Re crate database Delete all tables and create them again
+     * */
+    public void deleteUsers() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        // Delete All Rows
+        db.delete(TABLE_USER, null, null);
+        db.close();
+
+        Log.d(TAG, "Deleted all user info from sqlite");
+    }
+
+}
